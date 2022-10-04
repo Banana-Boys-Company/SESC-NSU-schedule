22d84a924bc1ec64cb348ebf81e80dc4e6437437
nekirik <112957792+nekirik@users.noreply.github.com>
2022-10-04 23:02:26 +0700

    Rare Update
    Подключили Шаблнизатор JS
    Фикс бага с объединением ячеек
    Фикс бага с вызовом нескольких таблиц

--- static/js/main.js
+++ static/js/main.js
@@ -13,6 +13,17 @@ socket.on('connect', () => {
     socket.send({ 'status': 200 })
 })
 
+let time_lst = ["1(8:30)", "2(9:15)", "3(10:20)", "4(11:25)", "5(12:30)", "6(13:25)"]
+
+let weekDay_title = [
+'    <div class="tab-pane fade show active" id="pills-Monday" role="tabpanel" aria-labelledby="pills-Monday-tab">',
+'    <div class="tab-pane fade" id="pills-Tuesday" role="tabpanel" aria-labelledby="pill-Tuesday-tab">',
+'    <div class="tab-pane fade" id="pills-Wednesday" role="tabpanel" aria-labelledby="pill-Wednesday-tab">',
+'    <div class="tab-pane fade" id="pills-Thursday" role="tabpanel" aria-labelledby="pill-Thursday-tab">',
+'    <div class="tab-pane fade" id="pills-Friday" role="tabpanel" aria-labelledby="pill-Friday-tab">',
+'    <div class="tab-pane fade" id="pills-Saturday" role="tabpanel" aria-labelledby="pill-Saturday-tab">'
+]
+
 // Код после полной загрузки страницы
 $(document).ready(function () {
     // Выбор группы класса
@@ -30,25 +41,39 @@ $(document).ready(function () {
 
 
     // Нажатие на дочерние меню 9 класса
+
+
+    let toggle_1 = 0;
+    let toggle_2 = 0;
+    let toggle_3 = 0;
     $(".child-toggle-1").on("click", function () {
-        $(".child-window-menu-1").toggle();
-        $(".child-window-menu-2").hide();
-        $(".child-window-menu-3").hide();
-  });
+        if (toggle_1>= 1) {
+            $(".child-window-menu-1").toggle();
+            $(".child-window-menu-2").hide();
+            $(".child-window-menu-3").hide();
+        }
+        toggle_1++;
+    });
 
     // Нажатие на дочерние меню 10 класса
-     $(".child-toggle-2").on("click", function () {
-        $(".child-window-menu-2").toggle();
-        $(".child-window-menu-1").hide();
-        $(".child-window-menu-3").hide();
-  });
+    $(".child-toggle-2").on("click", function () {
+        if (toggle_2 >= 1) {
+            $(".child-window-menu-2").toggle();
+            $(".child-window-menu-1").hide();
+            $(".child-window-menu-3").hide();
+        }
+        toggle_2++;
+    });
 
     // Нажатие на дочерние меню 11 класса
-      $(".child-toggle-3").on("click", function () {
-        $(".child-window-menu-3").toggle();
-        $(".child-window-menu-1").hide();
-        $(".child-window-menu-2").hide();
-  });
+    $(".child-toggle-3").on("click", function () {
+        if (toggle_3 >= 1) {
+            $(".child-window-menu-3").toggle();
+            $(".child-window-menu-1").hide();
+            $(".child-window-menu-2").hide();
+        }
+        toggle_3++;
+    });
 
 
     // Перезапуск таймера при активности
@@ -65,13 +90,75 @@ socket.on('schedule', data => {
     console.log(data)
     if (data["status"] === 200) {
         console.log("Connection success!")
+        data = {"monday": [['sksksk', 1], ['sksksk', 1], ['sksksk', 1], ['sksksk', 1], ['sksksk', 1], ['sksksk', 1]],
+        "tuesday": [['sksksk', 1], ["srgjmsrklgs'rk", 1], ['sksksk', 1], ['sksksk', 1], ['sksksk', 1], ['sksksk', 1]],
+        "wednesday": [['sksksk', 2], ['sksksk', 2], ['sksksk', 2]],
+        "thursday": [['sksksk', 1], ["srgjmsrklgs'rk", 1], ['sksksk', 1], ['sksksk', 1], ['sksksk', 1], ['sksksk', 1]],
+        "friday": [['sksksk', 1], ["srgjmsrklgs'rk", 1], ['sksksk', 1], ['sksksk', 1], ['sksksk', 1], ['sksksk', 1]],
+        "saturday": [['sksksk', 2], ['sksksk', 1], ['sksksk', 1], ['sksksk', 2]]
     }
-    //generate_schedule_table(data)
+    generate_schedule_table(data, weekDay_title, time_lst)
+}
 })
 
 // Генерация расписания на основе данных с сервера
-// function generate_schedule_table(data) {
-//     console.log("Generating shedule table...")
-//     ...
-// }
+function generate_schedule_table(data, weekDay_title, time_lst) {
+    $('#main-page').empty()
+    let start = `<ul class="nav nav-pills mb-3" id="pills-tab" role="tablist">
+    <li class="nav-item" role="presentation">
+    <button class="nav-link active" id="pills-Monday-tab" data-bs-toggle="pill" data-bs-target="#pills-Monday"
+    type="button" role="tab" aria-controls="Monday" aria-selected="true">Пн</button>
+    </li>
+    <li class="nav-item" role="presentation">
+    <button class="nav-link" id="pills-Tuesday-tab" data-bs-toggle="pill" data-bs-target="#pills-Tuesday" type="button"
+    role="tab" aria-controls="week-Tuesday" aria-selected="false">Вт</button>
+    </li>
+    <li class="nav-item" role="presentation">
+    <button class="nav-link" id="pills-Wednesday-tab" data-bs-toggle="pill" data-bs-target="#pills-Wednesday"
+    type="button" role="tab" aria-controls="week-Wednesday" aria-selected="false">Ср</button>
+    </li>
+    <li class="nav-item" role="presentation">
+    <button class="nav-link" id="pills-Thursday-tab" data-bs-toggle="pill" data-bs-target="#pills-Thursday"
+    type="button" role="tab" aria-controls="week-Thursday" aria-selected="false">Чт</button>
+    </li>
+    <li class="nav-item" role="presentation">
+    <button class="nav-link" id="pills-Friday-tab" data-bs-toggle="pill" data-bs-target="#pills-Friday" type="button"
+    role="tab" aria-controls="week-Friday" aria-selected="false">Пт</button>
+    </li>
+    <li class="nav-item" role="presentation">
+    <button class="nav-link" id="pills-Saturday-tab" data-bs-toggle="pill" data-bs-target="#pills-Saturday"
+    type="button" role="tab" aria-controls="week-Saturday" aria-selected="false">Сб</button>
+    </li>
+    </ul>
+    <div class="tab-content" id="weeks-tabContent">`
+    counter = 0
+    for (let i = 0; i <= 5; i++) {
+        start += weekDay_title[i]
+        start += '<table class="table table-bordered"><thead><tr><th scope="col" id="time">№ Урока</th><th scope="col">Урок</th></tr></thead><tbody>'
+        timer = 0
+        // bug spot
+        for (let element in data[Object.keys(data)[i]]) {            
+            let shablon = ""
+            shablon += `<tr><th scope="row" id="time">${time_lst[timer]}</th>`
+            timer += 1
+            console.log(data[Object.keys(data)[i]])
+            console.log(data[Object.keys(data)[i]][element])
+            if (data[Object.keys(data)[i]][element][1] > 1) {
+                shablon += `<td rowspan="${data[Object.keys(data)[i]][element][1]}" style="vertical-align: middle;">${data[Object.keys(data)[i]][element][0]}</td></tr>`
+                while (data[Object.keys(data)[i]][element][1] > 1){
+                    shablon += `<tr><th scope="row" id="time">${time_lst[timer]}</th></tr>`
+                    timer += 1
+                    data[Object.keys(data)[i]][element][1] -= 1}
+                }
+                else {
+                    shablon += `<td rowspan="1">${data[Object.keys(data)[i]][element][0]}</td></tr>`
+                }
+                start += shablon
+            }
+            start += "</tbody></table></div>"
+        }
+        // end bug
+        start += "</div>"
+        $('#main-page').append(start)
 
+    }
