dcac961f3816461b7258fc81fbb5d98ed1556650
whiteevilbro <maks-yakovlev21@yandex.ru>
2022-10-26 17:20:49 +0700

    courses parsing added

--- parser/parser.py
+++ parser/parser.py
@@ -10,10 +10,10 @@ import io
 import progress.bar
 try:
     from parser.common import pattern, dow2dow, subj, validate_str, get_merged_cell_val, full_subj_name, \
-        first_table_properties, ScheduleProperties, second_table_properties, merge_dicts, ProgressPlug
+        first_table_properties, ScheduleProperties, second_table_properties, merge_dicts, ProgressPlug, department_to_id
 except ImportError:
     from common import pattern, dow2dow, subj, validate_str, get_merged_cell_val, full_subj_name, \
-        first_table_properties, ScheduleProperties, second_table_properties, merge_dicts, ProgressPlug
+        first_table_properties, ScheduleProperties, second_table_properties, merge_dicts, ProgressPlug, department_to_id
 
 
 class ScheduleParser:
@@ -35,7 +35,7 @@ class ScheduleParser:
 
     def parse(self, sheet_name: str, _prop: ScheduleProperties, fp: IO[str] = None, bar_is_on: bool = False):
 
-        # Loading xlsx file
+        # region Loading xlsx file
         if sheet_name in self.wb.sheetnames:
             ws = self.wb[sheet_name]
         else:
@@ -43,8 +43,8 @@ class ScheduleParser:
         if fp:
             if not re.match(r'[+w]t?', fp.mode):
                 raise io.UnsupportedOperation('File is not writable')
-
-        # Initiating variables
+        # endregion
+        # region Initiating variables
         self.__set_prop(_prop)
         classes = dict()
         same_lesson = False
@@ -55,8 +55,8 @@ class ScheduleParser:
             bar = progress.bar.ChargingBar(f'Parsing {sheet_name}', max=_prop.progress)
         else:
             bar = ProgressPlug
-
-        # Generating dictionary structure
+        # endregion
+        # region Generating dictionary structure
         for i, column in enumerate(ws.iter_cols(max_row=2)):
             __v = str(get_merged_cell_val(ws, column[0]))
             __val = validate_str(get_merged_cell_val(ws, column[1]))
@@ -67,13 +67,13 @@ class ScheduleParser:
             classes.setdefault(__v, dict())
             classes[__v].setdefault(__val, dict())
             bar.next()
-
+        # endregion
         # They`re not needed anymore
         del __v, i, __val, column
 
-        # Starting parsing data
+        # region Parsing data
         for i, column in enumerate(ws.iter_cols(min_row=1, max_row=self.prop.last_row)):
-            # First column. Data - days of week. Creating dictonary {row: day_of_week}
+            # region First column. Data - days of week. Creating dictonary {row: day_of_week}
             if i == 0:
                 for row_, cell_ in enumerate(column[2:]):
                     bar.next()
@@ -87,13 +87,16 @@ class ScheduleParser:
                         row_to_day_of_the_week[cell_.row] = value
                         continue
                     row_to_day_of_the_week[cell_.row] = dow2dow[value]
-            # Useless columns
+            # endregion
+            # region Useless columns
             elif i in self.prop.useless_columns:
                 continue
-            # Third column. Data - time of lessons. Not used
+            # endregion
+            # region Third column. Data - time of lessons. Not used
             elif (i == 2) and self.prop.second_time:
                 for row_ in range(2, 13, 2):
                     times.append(str(column[row_].value)[:-3])
+            # endregion
             else:
                 # Creating local copy of dictionary with days of week as keys
                 __dict = copy.deepcopy(pattern)
@@ -107,7 +110,7 @@ class ScheduleParser:
                 if len(group_num) < 3:
                     group_num = '0' * (3 - len(group_num)) + group_num
 
-                # Looking through every cell
+                # region Looking through every cell
                 for row, cell in enumerate(column[2:]):
                     # Getting value of cell
                     val = validate_str(get_merged_cell_val(ws, cell))
@@ -150,30 +153,110 @@ class ScheduleParser:
                     # Cheking if this row was the first part of multiple rows for one lesson
                     same_lesson = str(_val).strip('.').upper() in subj
                     previous_day = day
-
-                # Divide every lesson length (in rows) to get actual length of lesson
+                # endregion
+                # region Divide every lesson length (in rows) to get actual length of lesson
                 for __day in __dict:
                     _day = __dict[__day]
                     for __lesson in _day:
                         __lesson[1] //= 2
                         bar.next()
+                # endregion
 
                 # Writing data in main dictionary
                 classes[class_num][group_num] = __dict
-
+        # endregion
         bar.finish()
 
-        # Returning json with unicode characters
+        # region Returning json with unicode characters
         if fp:
             dump(classes, fp=fp, ensure_ascii=False)
         return copy.deepcopy(classes)
+        # endregion
+
+    def parse_cources(self, sheet_name: str, fp: IO[str] = None):
+        pat = {
+            '1600': [],
+            '1800': [],
+            '2000': [],
+            '2030': [],
+            }
+
+        patte = {
+            'monday': {},
+            'tuesday': {},
+            'wednesday': {},
+            'thursday': {},
+            'friday': {},
+            'saturday': {}
+            }
+
+        # region Loading xlsx file
+        if sheet_name in self.wb.sheetnames:
+            import openpyxl.worksheet.worksheet
+            ws: xl.worksheet.worksheet.Worksheet = self.wb[sheet_name]
+        else:
+            raise SheetTitleException('Sheet with this name does not exist')
+        if fp:
+            if not re.match(r'[+w]t?', fp.mode):
+                raise io.UnsupportedOperation('File is not writable')
+        # endregion
+
+        courses = dict()
+        departments = []
+        times = []
+
+        for i, column in enumerate(ws.iter_cols(min_col=1, max_col=8, min_row=1, max_row=84)):
+            column: list[openpyxl.cell.Cell]
+            if i == 0:
+                departments = list(column[:])
+                continue
+            if i == 1:
+                times = column[1:]
+                continue
+            day = dow2dow[column[0].value.capitalize()]
+            # print(day)
+            # print(len(departments), '\n', column)
+            for row, cell in enumerate(column[1:]):
+                if row == 0:
+                    continue
+                department = department_to_id[get_merged_cell_val(ws, departments[row])]
+                # print(department)
+                if get_merged_cell_val(ws, times[row]) is None:
+                    continue
+                time = get_merged_cell_val(ws, times[row]).strip().rstrip()
+                time = time[:2] + time[3:]
+                # print(time)
+                try:
+                    _ = courses[department]
+                except KeyError:
+                    courses[department] = copy.deepcopy(patte)
+                try:
+                    _ = courses[department][day][time]
+                except KeyError:
+                    courses[department][day] = copy.deepcopy(pat)
+                if cell.value is None:
+                    continue
+                courses[department][day][time].append(cell.value)
+                # try:
+                #     if courses[department][day][time] == [None]:
+                #         continue
+                #     courses[department][day][time].append(cell.value)
+                #     # print(1)
+                # except KeyError:
+                #     # print(2)
+                #     courses[department][day][time] = copy.deepcopy([cell.value])
+                # print(courses[department][day][time])
+        return courses
+
+
 
 
 if __name__ == '__main__':
     p = ScheduleParser('SESC_Timetable 2022_2023.xlsx')
 
-    json = p.parse('Расписание_1 сем', first_table_properties, bar_is_on=True)
-    json2 = p.parse('Расписание_1сем_2пол.дня', second_table_properties, bar_is_on=True)
-    alld = merge_dicts(json, json2)
+    # json = p.parse('Расписание_1 сем', first_table_properties, bar_is_on=True)
+    # json2 = p.parse('Расписание_1сем_2пол.дня', second_table_properties, bar_is_on=True)
+    json = p.parse_cources('СПЕЦКУРСЫ')
+    # alld = merge_dicts(json, json2)
     with open('json.json', 'w') as file:
-        dump(alld, fp=file, ensure_ascii=False)
+        dump(json, fp=file, ensure_ascii=False)
